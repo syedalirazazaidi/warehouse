3cf104693683561a5815184ac19cb6021dbffdc7
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2024-01-14 22:49:18 +0500

    add new component

--- components/pages/news/professional.tsx
+++ components/pages/news/professional.tsx
@@ -2,6 +2,7 @@ import { Card } from "@/components/ui/card";
 import React from "react";
 import Image from "next/image";
 import ware from "../../../public/ware1.svg";
+import { Button } from "@/components/ui/button";
 export default function Professional() {
   return (
     <section className="text-gray-600 body-font">
@@ -11,19 +12,34 @@ export default function Professional() {
       <div className="container mx-auto mt-2">
         <div className="h-1 w-32 bg-[#A93838] rounded" />
       </div>
-      <div className="container px-5 mt-6 pb-24 mx-auto flex flex-wrap items-center">
+      <div className="container gap-4 px-5 mt-6 pb-24 mx-auto flex flex-wrap ">
+        <div className=" my-4">
+          <div className="border-2 bg-[#A93838] border-[#A93838] w-12 p-2  rounded-lg">
+            <h2 className="text-white  text-[12px] font-semibold  text-center items-center">
+              20 <br />
+              JULY
+            </h2>
+          </div>
+        </div>
         <div className="p-4 border-2 lg:w-[634px] md:w-[1/2] ">
           <Image src={ware} alt="warehouse" />
           <p className="text-[#222222] my-2 font-medium text-[14px]">
             Lorem ipsum dolor sit amet dolor sit amet{" "}
           </p>
           <div>
-            <div className="flex">
-              <p className="text-[14px] text-[#515151] font-normal ">About: </p>
-              <div className="flex gap-2 ml-2  text-[#A93838] font-semibold text-[14px]">
-                <p>Logistics,</p>
-                <p>Logistics</p>
+            <div className="flex justify-between">
+              <div className="flex ">
+                <p className="text-[14px] text-[#515151] font-normal ">
+                  About:{" "}
+                </p>
+                <p className="flex gap-2 ml-2  text-[#A93838] font-semibold text-[14px]">
+                  Logistics,
+                </p>
+                <p className="flex gap-2 ml-2  text-[#A93838] font-semibold text-[14px]">
+                  Logistics
+                </p>
               </div>
+              <div>by:John Doe</div>
             </div>
           </div>
           <p className="w-[624px] font-normal text-[14px] text-[#222222] mt-4">
@@ -57,6 +73,28 @@ export default function Professional() {
             <p className="font-semibold text-[14px] text-[#222222]">
               Search by tags
             </p>
+            <div className="flex flex-wrap w-[220px] gap-3 my-8">
+              <Button className="w-[62px] h-[28px] rounded-2xl bg-[#F0F0F0] font-normal text-[10px] text-[#222222]">
+                Advices
+              </Button>
+              <Button className="w-[62px] h-[28px] rounded-2xl bg-[#F0F0F0] font-normal text-[10px] text-[#222222]">
+                Freight
+              </Button>
+              <Button className="w-[62px] h-[28px] rounded-2xl bg-[#F0F0F0] font-normal text-[10px] text-[#222222]">
+                News
+              </Button>
+              <div className="flex gap-3">
+                <Button className="w-[62px] h-[28px] rounded-2xl bg-[#F0F0F0] font-normal text-[10px] text-[#222222]">
+                  Advices
+                </Button>
+                <Button className="w-[62px] h-[28px] rounded-2xl bg-[#F0F0F0] font-normal text-[10px] text-[#222222]">
+                  Freight
+                </Button>
+                <Button className="w-[62px] h-[28px] rounded-2xl bg-[#F0F0F0] font-normal text-[10px] text-[#222222]">
+                  News
+                </Button>
+              </div>
+            </div>
           </div>
           <div>
             <h2 className="mb-2 text-lg text-[14px] font-semibold text-[#222222]">
